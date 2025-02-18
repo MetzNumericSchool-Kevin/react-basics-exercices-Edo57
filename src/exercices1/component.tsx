e65027040812685627d665e4e3008e4281cf43a3
Edo57 <99027738+Edo57@users.noreply.github.com>
2025-02-18 14:34:46 +0100

    exercice1

---
+++ src/exercices1/component.tsx
@@ -0,0 +1,11 @@
+export default function Exercice1() {
+    return (
+      <section id="exercice1">
+        <h1>Boutique d'Archibald le Sorcier 🧙‍♂️</h1>
+        <p>
+          Bienvenue Aventurier, ici vous pouvez acheter diverses potions pour vos
+          aventures !
+        </p>
+      </section>
+    );
+  }
